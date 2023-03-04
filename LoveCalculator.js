b028ab53b0bd5e0f5ed0e4f5aeaaccca91e6c9c4
Mohammad Aazen <66300197+Aazen45v@users.noreply.github.com>
2023-03-05 01:48:58 +0530

    Create LoveCalculator.js

---
+++ LoveCalculator.js
@@ -0,0 +1,9 @@
+function loveCalculator(){
+ var yourName = prompt("Your Name");
+ var otherName = prompt("His/Her name");
+  var random = Math.random()*100;
+  random = Math.floor(random)+1;
+  console.log(yourName + "and" + otherName + "has a love percentage of = " + random + "%");
+}
+
+loveCalculator();
