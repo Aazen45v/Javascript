012fa392a4dbe5eb843ca9544b0071f03261e416
Mohammad Aazen <66300197+Aazen45v@users.noreply.github.com>
2023-03-05 23:41:46 +0530

    Update LoveCalculator.js

--- LoveCalculator.js
+++ LoveCalculator.js
@@ -5,10 +5,14 @@ function loveCalculator(){
   random = Math.floor(random)+1;
 
   if(random > 70){
-    alert (yourName + "and" + otherName + "has a love percentage of = " + random + "% : You love each other like juliet and romeo");
-  } else{
-    alert (yourName + "and" + otherName + "has a love percentage of = " + random + "%");
+    alert (yourName + " and " + otherName + " has a love percentage of = " + random + "% : You love each other like juliet and romeo");
   }
+   if(random > 30 && random <= 70){
+    alert (yourName + " and " + otherName + " has a love percentage of = " + random + "% : You two get going");
+    }
+   if(random <= 30){
+    alert (yourName + " and " + otherName + " has a love percentage of = " + random + "%");
+ }
 }
 
 loveCalculator();
